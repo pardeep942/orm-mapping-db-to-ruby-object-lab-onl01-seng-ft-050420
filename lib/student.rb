f8acc218c18007e514edd66227be431503f049f2
PARDEEP KAUR <pkaur84@aol.com>
2020-06-07 17:14:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -80,7 +80,15 @@ def self.students_below_12th_grade
   end
   
   def self.first_X_students_in_grade_10(number)
-  
+  sql = <<-SQL
+  SELECT * from students WHERE grade < 12;
+  SQL
+DB[:conn].execute(sql)
 end
+end
+
+
+
+
   
     
