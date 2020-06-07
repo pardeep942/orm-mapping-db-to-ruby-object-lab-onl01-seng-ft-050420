123e7da7dcfc1885cdb4667618d2611a8d2ec9a1
PARDEEP KAUR <pkaur84@aol.com>
2020-06-07 16:12:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,32 +1,43 @@
 class Student
-  
-  class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database	
-    student = Student.new
-    student.id = row[0]
-    student.name = row[1]
-    student.grade = row[2]
-    student
+    # create a new Student object given a row from the database
+  end
+
+  def self.all
+    # retrieve all the rows from the "Students" database
+    # remember each row should be a new instance of the Student class
+  end
+
+  def self.find_by_name(name)
+    # find the student in the database given a name
+    # return a new instance of the Student class
   end
   
-  def self.all
+  def save
     sql = <<-SQL
-      SELECT * FROM students;
-     SQL
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end
+      INSERT INTO students (name, grade) 
+      VALUES (?, ?)
+    SQL
 
-  def self.all_students_in_grade_9
-    sql = <<-SQL
-    SELECT * FROM students WHERE grade = 9;
-     SQL
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    
-    end 
+    DB[:conn].execute(sql, self.name, self.grade)
   end
-    
+  
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+    )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS students"
+    DB[:conn].execute(sql)
+  end
+end
