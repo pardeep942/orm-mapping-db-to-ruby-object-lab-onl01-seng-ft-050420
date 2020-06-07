8d30308a758d80ecc564a5d20f9f39b6632b82d4
PARDEEP KAUR <pkaur84@aol.com>
2020-06-07 16:52:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,7 +62,7 @@ end
     DB[:conn].execute(sql)
   end
   
-  def self.count_all_students_in_grade_9
+  def self.all_students_in_grade_9
     sql = <<-SQL
        SELECT COUNT(grade = 9) FROM * students
     SQL
