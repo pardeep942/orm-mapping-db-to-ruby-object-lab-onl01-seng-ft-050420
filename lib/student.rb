70fc1667069f58a84a9d60583ebeedf8781f5913
PARDEEP KAUR <pkaur84@aol.com>
2020-06-07 17:26:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -97,12 +97,12 @@ def self.students_below_12th_grade
     self.new_from_db(row)
   end
   
-  def self.all_students_in_grade_X(grade).map do |row|
+  def self.all_students_in_grade_X(grade)
     sql = <<-SQL
       SELECT * FROM students WHERE grade = ?  ORDER BY students.grade LIMIT 1;
     SQL
 
-    DB[:conn].execute(sql, grade).first
+    DB[:conn].execute(sql, grade).map do |row|
       self.new_from_db(row)
       
     end
