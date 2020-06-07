96d1595455f5755d5927ec43e9a973c9599feac1
PARDEEP KAUR <pkaur84@aol.com>
2020-06-07 17:30:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -81,7 +81,7 @@ def self.students_below_12th_grade
   
   def self.first_X_students_in_grade_10(number)
   sql = <<-SQL
-  SELECT * FROM students WHERE grade = 10 limit ?;
+  SELECT * FROM students WHERE grade = 10 limit = ?
   SQL
   DB[:conn].execute(sql, x)
     end
