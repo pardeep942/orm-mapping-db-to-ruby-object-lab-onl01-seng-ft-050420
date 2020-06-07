1fd1e4cc5924b56b401d44457b01958e55473e8f
PARDEEP KAUR <pkaur84@aol.com>
2020-06-07 17:46:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -80,13 +80,13 @@ def self.students_below_12th_grade
     
   end
   
-  def self.first_X_students_in_grade_10
-  sql = <<-SQL
-  SELECT * FROM students WHERE grade = 10 limit ?
-  SQL
-  DB[:conn].execute(sql).collect {|row| self.new_from_db(row)}
-  
-    end
+   def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = 10 LIMIT ?
+    SQL
+
+    DB[:conn].execute(sql, x).collect {|row| self.new_from_db(row)}
+  end
     
     
     def self.first_student_in_grade_10
